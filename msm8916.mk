1a1b2eced58fb53bd628f5c6ed903fae49f6470b
Akianonymus <anonymus.aki@gmail.com>
2018-08-20 06:16:57 +0530

    [TEMP]a6020: Transition to LD_SHIM_LIBS

--- msm8916.mk
+++ msm8916.mk
@@ -171,16 +171,6 @@ BOARD_PROVIDES_LIBRIL := false
 #include device/qcom/sepolicy/legacy-sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
-# Shims
-TARGET_LD_SHIM_LIBS := \
-    /system/lib64/libril.so|libshim_ril.so \
-    /system/vendor/lib/libmmcamera2_stats_modules.so|libshim_gui.so \
-    /system/vendor/lib/libmmcamera2_stats_modules.so|libshim_atomic.so \
-    /system/lib/hw/camera.vendor.msm8916.so|libshim_atomic.so \
-    /system/lib/libmmjpeg_interface.so|libshim_atomic.so \
-    /system/vendor/lib64/libril-qc-qmi-1.so|rild_socket.so \
-    /system/vendor/lib/libmmcamera2_imglib_modules.so|libshim_atomic.so
-
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn
