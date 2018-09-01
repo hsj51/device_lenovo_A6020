a4026a9015376bee2d6dbbc66d22defe115221aa
Daniel Jarai <jaraidaniel@gmail.com>
2018-09-02 00:34:14 +0200

    A6020: Make the external camera provider ignore internal cameras
    * The external camera provider occupies our camera v4l2 nodes,
      potentially crashing the kernel driver and blocking the camera HAL.
      Unfortunately, there's no easy way to just disable it, so let's just
      tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -11,3 +11,6 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
         /system/bin/cameraserver=23 \
 	/system/vendor/bin/mm-qcamera-daemon=23
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
