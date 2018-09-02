e961142c65214fddad56e0d69d2968ebffd127eb
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-09-02 22:00:40 +0530

    A6020: Update and binderize few HIDLs
    * Also dont simply copy manifest.xml to /system/vendor, instead let
      the build system do it by setting DEVICE_MANIFEST_FILE

---
+++ board/hidl.mk
@@ -0,0 +1,2 @@
+# HIDL
+DEVICE_MANIFEST_FILE := device/lenovo/A6020/manifest.xml
--- product/hidl.mk
+++ product/hidl.mk
@@ -1,21 +1,18 @@
 # HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
-
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
-#Audio
+# Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl
 
-#Bluetooth
+# Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -25,10 +22,10 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -38,24 +35,29 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
+    android.hardware.renderscript@1.0-impl \
+    android.hardware.renderscript@1.0-service
 
-#Sensors
+# Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
 
@@ -63,15 +65,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
 
-#Wifi
+# Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
 
-#USB service
+# USB service
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
-
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
