a46543f080754fc3f20a35e1cb4f875e3bad3b30
Sumit Das <sumidas@codeaurora.org>
2018-11-26 15:01:19 -0800

    A6020: FR53248: Android Q: Remove hidden API references in ims.apk
    Add telephony-qti-utils libarary to product packages
    
    Change-Id: Ie8651ebadc246c21bd5eb69ef060fe32d8f5f7c8

--- device.mk
+++ device.mk
@@ -258,6 +258,8 @@ PRODUCT_ENFORCE_RRO_TARGETS += \
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
