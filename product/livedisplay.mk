a0a379ac2eb14a5d55f5085e58a06376488a8923
Hrutvik <hrutvikjagtap51.com>
2019-03-24 13:12:16 +0530

    A6020: Build livedisplay 2.0 and sysfs service

--- product/livedisplay.mk
+++ product/livedisplay.mk
@@ -1,3 +1,5 @@
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-legacymm
+    vendor.lineage.livedisplay@1.0-service-legacymm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
