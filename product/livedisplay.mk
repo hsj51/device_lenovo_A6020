08cce626bdeccad77fe24d9a82b1e0b3ee55b8d0
hsj51 <hrutvikjagtap51@gmail.com>
2019-06-02 00:15:21 +0530

    A6020: Build livedisplay 2.0 and sysfs service

--- product/livedisplay.mk
+++ product/livedisplay.mk
@@ -1,3 +1,7 @@
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-legacymm
+    vendor.lineage.livedisplay@2.0-service-legacymm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
+PRODUCT_PACKAGES += \
+    libtinyxml
