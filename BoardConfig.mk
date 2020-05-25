51fc1ff7db7f92e9767f09d551c123b67a64e1d7
115ek <e.blosz@hotmail.de>
2020-05-25 15:35:10 +0100

    A6020: remove TARGET_KEYMASTER_WAIT_FOR_QSEE
    Change-Id: Ib64991c815a9551dba182ad8906f06e5f3dc9317

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,7 +116,6 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
 
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
-TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
