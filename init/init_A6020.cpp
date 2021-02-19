fb2eb6e4929541f797588ea3d6409f3170db8c84
Hrutvik Jagtap <hrutvikjagtap51@gmail.com>
2021-02-19 23:29:11 +0530

    A6020: Set properties related to ril that failed to set

--- init/init_A6020.cpp
+++ init/init_A6020.cpp
@@ -186,4 +186,7 @@ void vendor_load_properties()
     // Init a dummy BT MAC address, will be overwritten later
     property_override("ro.boot.btmacaddr", "00:00:00:00:00:00");
 
+    //Ril
+    property_override("ro.ril.svlte1x", "true");
+    property_override("ro.ril.svdo", "true");
 }
